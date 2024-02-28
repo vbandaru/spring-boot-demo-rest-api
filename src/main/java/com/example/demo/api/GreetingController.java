d577b3e4a03d7c7108c64c1378abda5fa02c0dcb
vbandaru <vijayender.bandaru@gmail.com>
2024-02-28 09:55:43 -0500

    add env var for demo

--- src/main/java/com/example/demo/api/GreetingController.java
+++ src/main/java/com/example/demo/api/GreetingController.java
@@ -2,17 +2,26 @@ package com.example.demo.api;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
+	
+	// demo env variable 
+    @Value("${user}")
+	private String user;
 
 	private final AtomicLong counter = new AtomicLong();
 
+
 	@GetMapping("/greeting")
-	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return new Greeting(counter.incrementAndGet(), String.format("Hello, %s!", name));
+	public Greeting greeting(@RequestParam(value = "name", required=false) String name ) {
+		
+		// use env var if no name is supplied
+		String input = name != null ?  name : user; 
+		return new Greeting(counter.incrementAndGet(), String.format("Hello, %s!", input));
 	}
 }
