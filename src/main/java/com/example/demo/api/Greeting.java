4ea557a869f69616e66ee4e70eb9f741a0b93371
Paul Jacobs <32575007+newpathexchange@users.noreply.github.com>
2024-02-29 22:53:53 -0500

    Modify the application to add a signature field and update K8s deployment

--- src/main/java/com/example/demo/api/Greeting.java
+++ src/main/java/com/example/demo/api/Greeting.java
@@ -1,14 +1,29 @@
 package com.example.demo.api;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.lang.IllegalStateException;
+
 public class Greeting {
 	
 	private long id;
 	
 	private String content;
+	private String sig;
 	
 	public Greeting(long id, String content) {
+		MessageDigest md5;
+		try {
+			md5 = MessageDigest.getInstance("MD5");
+			md5.reset();
+		} catch (NoSuchAlgorithmException e) {
+			throw new IllegalStateException(e.getMessage(), e);
+		}
+		md5.update(String.format("%d-%s", id, content).getBytes());
+
 		this.id = id;
 		this.content = content;
+		this.sig = md5.digest().toString().substring(3);
 	}
 	
 	public long getId() {
@@ -23,5 +38,12 @@ public class Greeting {
 	public void setContent(String content) {
 		this.content = content;
 	}
+	public String getSig() {
+		return sig;
+	}
+	public void setSig(String sig) {
+		this.sig = sig;
+	}
 
 }
+
